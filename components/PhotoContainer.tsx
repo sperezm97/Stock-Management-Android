ddb4a00886989e54bc876a46a8443ad5343ab9f0
juancarlosf2 <jceldominicano@gmail.com>
2020-07-25 14:53:59 -0400

    added new component called PhotoContainer

---
+++ components/PhotoContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+interface Props {}
+
+function PhotoContainer(props: Props) {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+export default PhotoContainer;
